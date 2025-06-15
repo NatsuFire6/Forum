e5fb043d02a7c656fbf789bbe28899b21cf3c37f
Timothée <timothee.sylla@ynov.com>
2025-06-15 19:58:30 +0200

    app.js WIP

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const postsRoutes = require('./routes/posts');
 
-app.use(express.json()); // pour lire le JSON dans les body
-app.use('/api/posts', postsRoutes);
+// Middleware
+app.use(cors());            // → autoriser les requêtes du front 
+app.use(express.json());    // pouvoir lire les requêtes en JSON tu connais
 
-app.listen(3000, () => {
-  console.log('Serveur lancé sur http://localhost:3000');
+// Routes
+const postRoutes = require('./routes/post');
+app.use('/api/posts', postRoutes); // → toutes requêtes /api/posts sera redirigé vers les routes post
+
+const userRoutes = require('./routes/user');
+app.use('/api/users', userRoutes); // bref t'a compris et on refait pareil pour tous le reste ↓
+
+const commentsRoutes = require('./routes/comments');
+app.use('/api/comments', commentsRoutes);
+
+const likesRoutes = require('./routes/likes');
+app.use('/api/likes', likesRoutes);
+
+const messagesRoutes = require('./routes/messages');
+app.use('/api/messages', messagesRoutes);
+
+
+// démarrer le serveur
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`serveur lancé sur le port http://localhost:${PORT}`);
 });
